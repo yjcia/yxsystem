36365fc47199e1ff79ae6be991bb0ee7bcae0aa0
jun.yan <857207947@qq.com>
2014-11-14 17:22:07 +0800

    update 2014-11-14

--- bower_components/bootstrap/dist/js/searchCondition.js
+++ bower_components/bootstrap/dist/js/searchCondition.js
@@ -22,8 +22,24 @@ function changeValue(newValue, currentId) {
                         "<select class='form-control' placeholder='Enter Category' " +
                         "id='chargeDescForCondition'>" + optionData + "</select>"));
             }
-        })
-
-
+        });
+    } else if (currentId.toString() == "amount") {
+        $("#queryConditionDiv").empty();
+        var amountRangeFrom = "<input type='number' class='form-control' id='amountFrom' " +
+            "placeholder='Enter Amount From'>";
+        var rangeSplit = " - ";
+        var amountRangeTo = "<input type='number' class='form-control' id='amountTo' " +
+            "placeholder='Enter Amount To'>";
+        $("#queryConditionDiv").append(amountRangeFrom + rangeSplit + amountRangeTo);
+    } else if (currentId.toString() == "date") {
+        $("#queryConditionDiv").empty();
+        var dateRangeFrom = "<div id = 'queryDateFrom' class='input-append date form_date ' data-date=''>"
+            + "<input class='form-control' size='16' type='text' value='' id='dateFrom'"
+            + "placeholder='Enter Date From'><span class='add-on'><i class='icon-th'></i></span></div>";
+        //var rangeSplit = " - ";
+        //var dateRangeTo = "<div id = 'queryDateTo' class='input-append date form_date ' data-date=''>"
+        //+"<input class='form-control' size='16' type='text' value='' id='dateTo'"
+        //+"placeholder='Enter Date To'><span class='add-on'><i class='icon-th'></i></span></div>";
+        $("#queryConditionDiv").append(dateRangeFrom);
     }
 }
